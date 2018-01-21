6276cda961ecddf6df04f9e0138414ee34e16d22
Marko <marko.lipka@posteo.de>
2018-01-21 02:04:36 +0100

    built a function to plot CTD data

--- plot.CTD.data.R
+++ plot.CTD.data.R
@@ -1,23 +1,17 @@
-source("read.cnv.files.R")
+
 library(reshape2)
 library(ggplot2)
 
-files <- list.files(path = "~/Dropbox/", pattern = "\\.cnv$", recursive = T, ignore.case = T)
-
-potential.id.vars <- c("depSM", "prDM", "pr", "scan", "nbin",
-                       "flag", "timeS", "nbf",
-                       "altM", "prDM", "spar",
-                       "header.latitude", "header.longitude",
-                       "latitude", "longitude")
-potential.depvars <- c("depSM", "prDM", "pr")
-
-for(file in files){
-    cat(paste(file, " ... \n"))
-    file.abs.path <- paste0("~/Dropbox/", file)
-    file.path.sub <- gsub("/", "_", file.abs.path)
-    import <- read.cnv.file(file.abs.path)
-    df   <- import$data
-    meta <- import$meta
+plot.CTD.data <- function(CTD.data){
+    df   <- CTD.data$data
+    meta <- CTD.data$meta
+    
+    potential.id.vars <- c("depSM", "prDM", "pr", "scan", "nbin",
+                           "flag", "timeS", "nbf",
+                           "altM", "prDM", "spar",
+                           "header.latitude", "header.longitude",
+                           "latitude", "longitude")
+    potential.depvars <- c("depSM", "prDM", "pr")
     
     try({
         melted.df <- melt(df, id.vars = names(df)[names(df) %in% potential.id.vars])
@@ -32,8 +26,27 @@ for(file in files){
             coord_flip() +
             facet_grid(.~variable, scales = "free") +
             theme_bw() +
+            ggtitle(paste(meta, collapse = ", ")) +
             theme(axis.text.x = element_text(angle = 45, hjust = 1))
-        ggsave(filename = paste0(file.path.sub, ".png"),
-               path = "~/cnv_test", width = 12, height = 4)})
-    cat("... done\n")
+    })
+    
 }
+
+
+
+# ### Run through all cnv-files in the Dropbox directory and try to plot them:
+# source("read.cnv.files.R")
+# 
+# files <- list.files(path = "~/Dropbox/", pattern = "\\.cnv$", recursive = T, ignore.case = T)
+# 
+# for(file in files){
+#     cat(paste(file, " ... \n"))
+#     file.abs.path <- paste0("~/Dropbox/", file)
+#     file.path.sub <- gsub("/", "_", file.abs.path)
+#     import <- read.cnv.file(file.abs.path)
+#     plot.CTD.data(import)
+#     ggsave(filename = paste0(file.path.sub, ".png"),
+#            path = "~/cnv_test", width = 12, height = 4)
+#     cat("... done\n")
+# }
+
