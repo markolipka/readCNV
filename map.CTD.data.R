da6c85d936d91c0e001680543a12c9d11e94875e
Marko Lipka <markolipka@posteo.de>
2021-05-05 21:59:36 +0200

    only re-indentation

--- map.CTD.data.R
+++ map.CTD.data.R
@@ -7,40 +7,40 @@ library(mapview)
 library(leafpop)
 
 map.CTD.data <- function(path.to.start = "example data") {
-
-files <- list.files(path = path.to.start, 
-                    pattern = "\\.cnv$",
-                    full.names = TRUE,
-                    recursive = T, ignore.case = T)
-
-data <- lapply(files, function(cnv){
-    d <- read.cnv.file(cnv)
-    if(all(!is.na(d$coords))){
-        coords <- d$coords %>%
-            as_tibble() %>%
-            mutate(LatDecDeg = parzer::parse_lat(Lat),
-                   LonDecDeg = parzer::parse_lon(Lon)) %>%
-            st_as_sf(coords = c("LonDecDeg", 
-                                "LatDecDeg"), 
-                     crs = 4326)
-    }else(coords <- NULL)
-    return(coords)
-}) %>%
-    bind_rows()
-
-plots <- lapply(files, function(cnv){
-    d <- read.cnv.file(cnv)
-    p <- plot.CTD.data(d)
-    if(all(!is.na(d$coords))){
-        return(p)
-    }
-})
-plots <- plots[!sapply(plots, is.null)]
-
-
-
-
-mapview(data, popup = popupGraph(plots, height = 200, width = 700))
+    
+    files <- list.files(path = path.to.start, 
+                        pattern = "\\.cnv$",
+                        full.names = TRUE,
+                        recursive = T, ignore.case = T)
+    
+    data <- lapply(files, function(cnv){
+        d <- read.cnv.file(cnv)
+        if(all(!is.na(d$coords))){
+            coords <- d$coords %>%
+                as_tibble() %>%
+                mutate(LatDecDeg = parzer::parse_lat(Lat),
+                       LonDecDeg = parzer::parse_lon(Lon)) %>%
+                st_as_sf(coords = c("LonDecDeg", 
+                                    "LatDecDeg"), 
+                         crs = 4326)
+        }else(coords <- NULL)
+        return(coords)
+    }) %>%
+        bind_rows()
+    
+    plots <- lapply(files, function(cnv){
+        d <- read.cnv.file(cnv)
+        p <- plot.CTD.data(d)
+        if(all(!is.na(d$coords))){
+            return(p)
+        }
+    })
+    plots <- plots[!sapply(plots, is.null)]
+    
+    
+    
+    
+    mapview(data, popup = popupGraph(plots, height = 200, width = 700))
 }
 
-map.CTD.data()
+# map.CTD.data()
